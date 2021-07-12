5bd9fe0b9f5892d440090baa8655f3ad97eabca1
Andre <andreyamasaki@brq.com>
2021-07-12 13:12:56 -0300

    implemented performRequest method to get data from coinAPI

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,33 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "5668BA67-D1DB-48F3-BC93-E3492182BB9D"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currencyArray[0])?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                print(error ?? "uh oh, something went wrong")
+                return
+                }
+                if let safeData = data {
+                    print(safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON() {
+        
+    }
 }
